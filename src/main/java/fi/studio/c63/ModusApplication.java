277303948666a4a74e18aabee50487d808a3d1d2
Tung Bui Duy <tung.duy@tappcommerce.com>
2016-10-29 15:19:55 +0300

    initialization

---
+++ src/main/java/fi/studio/c63/ModusApplication.java
@@ -0,0 +1,13 @@
+package fi.studio.c63;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ModusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModusApplication.class, args);
+    }
+}
---
+++ src/test/java/fi/studio/c63/ModusApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.studio.c63;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
